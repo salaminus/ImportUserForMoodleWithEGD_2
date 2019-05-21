8d0c61857237db617fc7f14c37afbf01b1346beb
salaminus <36138364+salaminus@users.noreply.github.com>
2019-05-21 10:12:38 +0300

    Last version program 21-05-2019

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ import openpyxl
 import transliterate
 
 
+kItem = 0                                       #кол-во записей
+listDataItems = []                              #список записей для экспорта в csv
+
+
 #TODO: Открытие файла Excel
-nameFileExcel = 'test.xlsx'
-wb = openpyxl.load_workbook(nameFileExcel)      #открытие книги Excel
+nameFileExcel = '10a-1'
+nameFullFileExcel = nameFileExcel + '.xlsx'
+wb = openpyxl.load_workbook(nameFullFileExcel)      #открытие книги Excel
 listSheets = wb.sheetnames                      #получение списка листов книги Excel
 sheets1 = wb[listSheets[0]]                     #берем первый лист книги Excel
 # print(sheets1.cell(row=3,column=2).value)     #получение содержимого ячейки
@@ -32,14 +37,40 @@ def translitNames(fio):
     return lastName, firstName
 
 def e_mailGet(lastName, firstName):
+    """получение e-mail"""
     e_mail = lastName + '.' + firstName + '@test.ru'
     return e_mail
 
 #TODO: Записать Ф и И в новый лист Excel и сохранить в csv
+def writeDataToFileCSV(listDataItems):
+    """запись данных в файл"""
+    nameResultFile = nameFileExcel + '.csv'
+    with open(nameResultFile,'w', encoding='WINDOWS-1251') as f:
+        f.write('username' + ';' +
+                'password' + ';' +
+                'firstname' + ';' +
+                'lastname' + ';' +
+                'email' + '\n')
+        for item in listDataItems:
+            f.write(item['username'] + ';' +
+                item['password'] + ';' +
+                item['firstname'] + ';' +
+                item['lastname'] + ';' +
+                item['email'] + '\n')
 
-kItem = 0                                       #кол-во записей
+#TODO: Записать данные в список listDataItem
+def addToListDataItem(rusLastName, rusFirstName, lastName, e_mail):
+    """создание словаря из rusLastName, rusFirstName, lastName, e_mail
+       и запись в список словарей listDataItems"""
+    dictDataItem = {}
+    dictDataItem['username'] = lastName
+    dictDataItem['password'] = lastName
+    dictDataItem['firstname'] = rusFirstName
+    dictDataItem['lastname'] = rusLastName
+    dictDataItem['email'] = e_mail
+    listDataItems.append(dictDataItem)
 
-for i in range(3,51):
+for i in range(3,34):
     # TODO: Получение столбца с ФИО
     valueCurrentCell = sheets1.cell(row=i,column=2).value
     if str(type(valueCurrentCell)) != "<class 'NoneType'>":
@@ -52,8 +83,12 @@ for i in range(3,51):
             firstName = translitNames(fio)[1]
             e_mail = e_mailGet(lastName, firstName)
             printInfoToConsole(rusLastName, rusFirstName, lastName, e_mail)
+            addToListDataItem(rusLastName, rusFirstName, lastName, e_mail)
             if e_mail:
                 kItem = kItem + 1
+
         except:
             pass
 print('Total: ' + str(kItem))
+writeDataToFileCSV(listDataItems)
+print(listDataItems)
